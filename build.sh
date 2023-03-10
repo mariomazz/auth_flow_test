a7ee6f8e64c080ed04494cfdac2078a7aea7eef5
mario <mario.mazzarelli@asfweb.net>
2023-03-10 15:41:28 +0100

    .

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 rm -rf dist
-flutter build web --base-href="/" --release && mkdir -p dist/web && cp -r build/web dist
+flutter build web --base-href="/" --release
+mkdir -p dist/web
+cp -r build/web dist
 cp dist/web/index.html dist/web/404.html
